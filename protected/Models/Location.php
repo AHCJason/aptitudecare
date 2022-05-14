acd477bc59ec3e12d135891b66d11e7f054ccab5
AHCJason <39806233+AHCJason@users.noreply.github.com>
2022-05-13 19:46:56 -0600

    Vouch baby steps, merged a few functions

--- protected/Models/Location.php
+++ protected/Models/Location.php
@@ -13,19 +13,7 @@ class Location extends AppData {
 		)
 	);
 
-
-	// fetch all the locations to which the user has permission to access. 
-	// this function is used on the mainpagecontroller
-	public function fetchAllLocations() {
-		$user = auth()->getRecord();
-		$userLocation = $this->loadTable('UserLocation');
-
-		$sql = "SELECT * FROM {$this->tableName()} INNER JOIN {$userLocation->tableName()} ON {$userLocation->tableName()}.location_id = {$this->tableName()}.id WHERE {$userLocation->tableName()}.user_id = :user_id";
-		$params[":user_id"] = $user->id;
-		return $this->fetchAll($sql, $params);
-	}
-
-
+	/* //don't believe in use
 	public function fetchDefaultLocation() {
 		if (auth()->valid()) {
 			$user = auth()->getRecord();
@@ -39,9 +27,9 @@ class Location extends AppData {
 
 		return $this->fetchOne($sql, $params);
 
-	}
+	}*/
 
-
+	/* //don't believe in use
 	public function fetchOtherLocations($module = null) {
 		if (auth()->valid()) {
 			$user = auth()->getRecord();
@@ -71,6 +59,7 @@ class Location extends AppData {
 		$params[":user_id"] = $user->id;
 		return $this->fetchAll($sql, $params);
 	}
+	*/
 
 	public function fetchLinkedFacilities($location_id = false) {
 		if (auth()->valid()) {
@@ -143,7 +132,7 @@ class Location extends AppData {
 		return $this->fetchOne($sql, $params);
 	}
 	
-	
+	//Fetches single location.
 	public function fetchLocation($id) {
 		$sql = "SELECT * FROM {$this->tableName()} WHERE {$this->tableName()}.";
 		
@@ -164,21 +153,118 @@ class Location extends AppData {
 	}
 
 
+	private function fetchLocationofType($type = null)
+	{
+		if(isset($_COOKIE['VouchCookie']))
+		{
+			$vc = auth()->VouchCookie();
+			if($vc->status == 200){
+				//get id from pubID in ac_user, lookup group_ids in ac_user_group, use group_id to get list of permission_id from ac_group_permission, user permission_id to get names of permissions from ac_permission.
+
+				$sql = "SELECT * FROM {$this->tableName()} WHERE {$this->tableName()}.location_type = 1 AND public_id IN (";
+				foreach($vc->Available_Loc as $key => $value) {
+					$sql .= ":Available_Loc$key, ";
+					$params[":Available_Loc$key"] = $value;
+				}
+				//fix hanging fencewire.
+				$sql = trim($sql, ", ");
+				$sql .= ")";
+				return db()->FetchRows($sql, $params, $this);
+			} else {
+				die("failed validation");
+			}
+		} else {
+			$params[":user_id"] = auth()->getRecord()->id;
+
+			$sql = "SELECT * FROM {$this->tableName()} INNER JOIN ac_user_location ON {$this->tableName()}.id = ac_user_location.location_id WHERE ac_user_location.user_id = :user_id";
+			if($type != null)
+			{
+				$sql .= " AND {$this->tableName()}.location_type = :type";
+				$params[":type"] = $type;
+			}
+			return $this->fetchAll($sql, $params);
+		}
+	}
+
+	public function fetchAllLocations() {
+		return $this->fetchLocationofType();
+	}
 
 	public function fetchHomeHealthLocations() {
-		$sql = "SELECT * FROM {$this->tableName()} LEFT JOIN ac_user_location ON ac_user_location.location_id = {$this->tableName()}.id WHERE ac_user_location.user_id = :id AND {$this->tableName()}.location_type = 2";
-		$params[":id"] = auth()->getRecord()->id;
+		return $this->fetchLocationofType(2);
+	}
+
+	public function fetchFacilities() {
+		return $this->fetchLocationofType(1);
+	}
+
+	/* comment out the old functions and merge to one shared function with type filters.
+	// fetch all the locations to which the user has permission to access. 
+	// this function is used on the mainpagecontroller
+	public function fetchAllLocations() {
+		$user = auth()->getRecord();
+		$userLocation = $this->loadTable('UserLocation');
+
+		$sql = "SELECT * FROM {$this->tableName()} INNER JOIN {$userLocation->tableName()} ON {$userLocation->tableName()}.location_id = {$this->tableName()}.id WHERE {$userLocation->tableName()}.user_id = :user_id";
+		$params[":user_id"] = $user->id;
 		return $this->fetchAll($sql, $params);
+	}
+
+
+	public function fetchHomeHealthLocations() {
+		if(isset($_COOKIE['VouchCookie']))
+		{
+			$vc = auth()->VouchCookie();
+			if($vc->status == 200){
+				//get id from pubID in ac_user, lookup group_ids in ac_user_group, use group_id to get list of permission_id from ac_group_permission, user permission_id to get names of permissions from ac_permission.
+
+				$sql = "SELECT * FROM {$this->tableName()} WHERE {$this->tableName()}.location_type = 2 AND public_id IN (";
+				foreach($vc->Available_Loc as $key => $value) {
+					$sql .= ":Available_Loc$key, ";
+					$params[":Available_Loc$key"] = $value;
+				}
+				//fix hanging fencewire.
+				$sql = trim($sql, ", ");
+				$sql .= ")";
+				return db()->FetchRows($sql, $params, $this);
+			} else {
+				die("failed validation");
+			}
+		} else {
+			$sql = "SELECT * FROM {$this->tableName()} LEFT JOIN ac_user_location ON ac_user_location.location_id = {$this->tableName()}.id WHERE ac_user_location.user_id = :id AND {$this->tableName()}.location_type = 2";
+			$params[":id"] = auth()->getRecord()->id;
+			return $this->fetchAll($sql, $params);
+		}
 	}
 
 
 	public function fetchFacilities() {
-		$params[":user_id"] = auth()->getRecord()->id;
+		if(isset($_COOKIE['VouchCookie']))
+		{
+			$vc = auth()->VouchCookie();
+			if($vc->status == 200){
+				//get id from pubID in ac_user, lookup group_ids in ac_user_group, use group_id to get list of permission_id from ac_group_permission, user permission_id to get names of permissions from ac_permission.
 
-		$sql = "SELECT * FROM {$this->tableName()} INNER JOIN ac_user_location ON {$this->tableName()}.id = ac_user_location.location_id WHERE {$this->tableName()}.location_type = 1 AND ac_user_location.user_id = :user_id";
-		return $this->fetchAll($sql, $params);
+				$sql = "SELECT * FROM {$this->tableName()} WHERE {$this->tableName()}.location_type = 1 AND public_id IN (";
+				foreach($vc->Available_Loc as $key => $value) {
+					$sql .= ":Available_Loc$key, ";
+					$params[":Available_Loc$key"] = $value;
+				}
+				//fix hanging fencewire.
+				$sql = trim($sql, ", ");
+				$sql .= ")";
+				return db()->FetchRows($sql, $params, $this);
+			} else {
+				die("failed validation");
+			}
+		} else {
+			$params[":user_id"] = auth()->getRecord()->id;
+
+			$sql = "SELECT * FROM {$this->tableName()} INNER JOIN ac_user_location ON {$this->tableName()}.id = ac_user_location.location_id WHERE {$this->tableName()}.location_type = 1 AND ac_user_location.user_id = :user_id";
+			return $this->fetchAll($sql, $params);
+		}
 	}
-
+	*/
 
 	public function fetchHealthcareFacilityId() {
 		$healthcareFacility = $this->loadTable("HealthcareFacility");
